d0d6bb3913bdaa7de65b3133600a6c64d39b478c
theogplant <snleaks@gmail.com>
2016-05-16 15:15:59 -0600

    Add files via upload
    gui so far (it doesn't do anything)

---
+++ Pong/src/com/neumont/csc150/view/PongGUI.java
@@ -0,0 +1,37 @@
+package com.neumont.csc150.view;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JPanel;
+
+public class PongGUI extends JPanel implements KeyListener{
+
+	private PongGame game;
+	private Ball ball;
+	private Paddle humanPaddle, computerPaddle;
+	private int humanScore, computerScore;
+	
+	public PongGUI(PongGame _game){
+		
+	}
+	
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
