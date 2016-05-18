c466954f7ca07f6dc487ba1f71f864ed4f8ca371
stmknight <amcazar@yahoo.com>
2016-05-18 00:39:08 -0600

    Update Ball.java

--- Pong/src/com/neumont/csc150/pong/model/Ball.java
+++ Pong/src/com/neumont/csc150/pong/model/Ball.java
@@ -1,5 +1,64 @@
 package com.neumont.csc150.pong.model;
 
-public class Ball {
+import java.awt.Color;
+import java.awt.Point;
+import java.util.Random;
 
+public class Ball {
+	private Random r = new Random();
+	private Color color;
+	private int yDelta = 1;
+	private int xDelta = 1;
+	private int size;
+	private Point position;
+	
+	public Ball(Point p){
+		yDelta = r.nextInt(6) - 3;
+		if(yDelta == 0)
+			yDelta++;
+		xDelta = r.nextInt(6) - 3;
+		if(xDelta == 0)
+			xDelta++;
+		color = new Color(200, 200, 200);
+		position = new Point(p);
+		size = 10;
+	}
+	
+//	public Ball(int size){
+//		color = new Color(200, 200, 200);
+//		position = new Point();
+//		this.size = size;
+//	}
+
+	public int getyDelta() {
+		return yDelta;
+	}
+
+	public void setyDelta(int yDelta) {
+		this.yDelta = yDelta;
+	}
+
+	public int getxDelta() {
+		return xDelta;
+	}
+
+	public void setxDelta(int xDelta) {
+		this.xDelta = xDelta;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	public Point getPosition() {
+		return position;
+	}
+
+	public void setPosition(Point position) {
+		this.position = position;
+	}
 }
