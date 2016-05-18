4c277b5fd5ac77023eddc15200816295ae8afcaf
stmknight <amcazar@yahoo.com>
2016-05-18 00:45:10 -0600

    Update Paddle.java

--- Pong/src/com/neumont/csc150/pong/model/Paddle.java
+++ Pong/src/com/neumont/csc150/pong/model/Paddle.java
@@ -1,23 +1,54 @@
 package com.neumont.csc150.pong.model;
 
-public class Paddle {
+import java.awt.Color;
+import java.awt.Point;
 
-	public int getYDelta() {
-		// TODO Auto-generated method stub
-		return 0;
+public class Paddle {
+	private Color color = new Color(200, 200, 200);
+	private int yDelta = 2;
+	private int height;
+	private static int width = 10;
+	private Point position;
+	
+	public Paddle(int height, Point p){
+		this.height = height;
+		position = new Point(p);
+	}
+	
+	public Paddle(Point p){
+		this.height = 75;
+		position = new Point(p);
 	}
 
-	public int getSpeed() {
-		// TODO Auto-generated method stub
-		return 0;
+	public int getyDelta() {
+		return yDelta;
+	}
+
+	public void setyDelta(int yDelta) {
+		this.yDelta = yDelta;
 	}
 
 	public int getHeight() {
-		// TODO Auto-generated method stub
-		return 0;
+		return height;
 	}
 
+	public void setHeight(int height) {
+		this.height = height;
+	}
 
-	
+	public int getWidth() {
+		return width;
+	}
 
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public Point getPosition() {
+		return position;
+	}
+
+	public void setPosition(Point position) {
+		this.position = position;
+	}
 }
